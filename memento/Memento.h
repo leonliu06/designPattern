1d1954db8c725a9c054fcd06abca8650170c9b96
leonliu06 <leonliu06@gmail.com>
2017-10-30 09:41:58 +0800

    memento

--- memento/Memento.h
+++ memento/Memento.h
@@ -7,11 +7,12 @@ typedef string State;
 
 class Memento {
 public:
+	~Memento();
+private:
 	//这是最关键的地方，将 Originator 声明为 friend 类，可以访问内部信息，但是其他类不能访问
 	friend class Originator;
 	Memento();
-	Memento(const State& sdt);
-	~Memento();
+	Memento(const State& sdt);	
 	void SetState(const State& sdt);
 	State GetState();
 private:
