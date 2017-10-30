0b02f19244e426e0075605d91728431b29f85210
leonliu06 <leonliu06@gmail.com>
2017-10-30 16:56:41 +0800

    observer

---
+++ observer/Observer.h
@@ -0,0 +1,42 @@
+//Observer.h
+#pragma once
+
+#include "Subject.h"
+#include <string>
+using namespace::std;
+typedef string State;
+
+class Observer {
+public:
+	virtual ~Observer();
+	virtual void Update(Subject* sub) = 0;
+	virtual void PrintInfo() = 0;
+protected:
+	Observer();
+	State _st;
+private:
+};
+
+class ConcreteObserverA : public Observer {
+public:
+	virtual Subject* GetSubject();
+	ConcreteObserverA(Subject* sub);
+	virtual ~ConcreteObserverA();
+	void Update(Subject* sub);
+	void PrintInfo();
+protected:
+private:
+	Subject* _sub;
+};
+
+class ConcreteObserverB : public Observer {
+public:
+	virtual Subject* GetSubject();
+	ConcreteObserverB(Subject* sub);
+	virtual ~ConcreteObserverB();
+	void Update(Subject* sub);
+	void PrintInfo();
+protected:
+private:
+	Subject* _sub;
+};
