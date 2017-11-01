93e8ed4a6629c769fd8ef41c17e5db10ca63dd77
leonliu06 <leonliu06@gmail.com>
2017-11-01 16:55:43 +0800

    visitor

---
+++ visitor/Visitor.h
@@ -0,0 +1,26 @@
+//Visitor.h
+#pragma once
+
+class ConcreteElementA;
+class ConcreteElementB;
+class Element;
+
+class Visitor {
+public:
+	virtual ~Visitor();
+	virtual void VisitConcreteElementA(Element* elm) = 0;
+	virtual void VisitConcreteElementB(Element* elm) = 0;
+protected:
+	Visitor();
+private:
+};
+
+class ConcreteVisitorA : public Visitor {
+public:
+	ConcreteVisitorA();
+	virtual ~ConcreteVisitorA();
+	virtual void VisitConcreteElementA(Element* elm);
+	virtual void VisitConcreteElementB(Element* elm);
+protected:
+private:
+};
